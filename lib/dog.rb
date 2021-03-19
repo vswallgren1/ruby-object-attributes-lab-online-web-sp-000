9cd301274ace18f3218018a2faf78a0bec8c2039
Vince W <vincewallgren@me.com>
2021-03-19 03:45:31 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,13 +2,16 @@ class Dog
   def name
     @name
   end
-  def name = (name)
+
+  def name=(name)
     @name = name
   end
+
   def breed
     @breed
   end
-  def breed = (breed)
+
+  def breed=(breed)
     @breed = breed
   end
 end
